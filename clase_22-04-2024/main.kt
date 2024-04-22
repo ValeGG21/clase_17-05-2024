4d5b4ae0d4607266f78a010ee2bfa5cfc1d11d32
Valeria <valegogo95@gmail.com>
2024-04-22 17:46:16 -0500

    Dependencias esperar

---
+++ clase_22-04-2024/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var saludo: String = "Hola a todos!";
+    var numero: Int? = 32;
+    println(saludo + ' ' + numero);
+    
+    val nombre:String? = readLine();
+    println(nombre);
+    
+    val edad = readLine();
+    val valor = edad.toInt();
+    println(valor);
+}
